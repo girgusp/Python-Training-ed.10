714db3520aa1f4ee4eee52cc7cb1db3c5db77913
Przemysław Girguś <girgus.przemyslaw@gmail.com>
2018-08-25 10:47:50 +0200

    nowy test 'test_edit_group'

--- fixture/group.py
+++ fixture/group.py
@@ -39,3 +39,20 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
+    def edit_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # open group to edit
+        wd.find_element_by_name("edit").click()
+        # edit fields
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").send_keys("_edited")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").send_keys("_edited")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").send_keys("_edited")
+        # submit edition
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
