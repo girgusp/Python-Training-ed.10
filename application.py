134c460c736e5e18ff7edd133be9848d3eee790f
Przemysław Girguś <girgus.przemyslaw@gmail.com>
2018-08-25 06:38:35 +0200

    ujednolicenie pliku 'application.py' dla scenariuszy dodawania grup oraz kontaktów

--- application.py
+++ application.py
@@ -54,5 +54,34 @@ class Application:
         wd = self.wd
         wd.get("http://localhost/addressbook/addressbook/")
 
+    def return_to_home_page(self):
+        wd = self.wd
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def create_contact(self, contact):
+        wd = self.wd
+        # open contacts page
+        self.open_contacts_page()
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        # submit contact creation
+        wd.find_element_by_name("theform").click()
+        # return to home page
+        self.return_to_home_page()
+
+    def open_contacts_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("add new").click()
+
     def destroy(self):
         self.wd.quit()
