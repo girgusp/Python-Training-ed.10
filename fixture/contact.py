1e58f910910c54092e8ab319c79a267c0a084646
Przemysław Girguś <girgus.przemyslaw@gmail.com>
2018-09-13 10:32:41 +0200

    commit dla konsultacji

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,3 +1,6 @@
+from model.contact import Contact
+
+
 class ContactHelper:
 
     def __init__(self, app):
@@ -86,5 +89,15 @@ class ContactHelper:
         wd = self.app.wd
         self.open_home_page()
         return len(wd.find_elements_by_name("selected[]"))
+
+    def get_contact_list(self):
+        wd = self.app.wd
+        self.open_home_page()
+        contacts = []
+        for element in wd.find_elements_by_name("entry"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            contacts.append(Contact(first_name=text, id=id))
+        return contacts
 
 
--- model/contact.py
+++ model/contact.py
@@ -1,6 +1,21 @@
+from sys import maxsize
+
 class Contact:
 
-    def __init__(self, first_name=None, middle_name=None, last_name=None):
+    def __init__(self, first_name=None, middle_name=None, last_name=None, id=None):
         self.first_name = first_name
         self.middle_name = middle_name
         self.last_name = last_name
+        self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.first_name)
+
+    def __eq__(self, other):
+        return (self.id is None or other.id is None or self.id == other.id) and self.first_name == other.first_name
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
