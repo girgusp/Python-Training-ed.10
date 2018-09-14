bb350f07c11bcbcb1cf41117bccb1e1706e9b99b
Przemysław Girguś <girgus.przemyslaw@gmail.com>
2018-09-14 11:51:02 +0200

    mały fix do zadania 12 (dodanie brakujących None przy haszach)

--- fixture/group.py
+++ fixture/group.py
@@ -39,13 +39,16 @@ class GroupHelper:
             wd.find_element_by_link_text("groups").click()
 
     def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
         self.select_first_group()
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
-        self.group_cache
+        self.group_cache = None
 
     def select_first_group(self):
         wd = self.app.wd
@@ -62,7 +65,7 @@ class GroupHelper:
         # submit modification
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
-        self.group_cache
+        self.group_cache = None
 
     def count(self):
         wd = self.app.wd
