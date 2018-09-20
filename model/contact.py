8e503446aaac619b23ea06e75ee0a442c4a97d58
Przemysław Girguś <girgus.przemyslaw@gmail.com>
2018-09-20 20:05:42 +0200

    po 5.4, ale są errory

--- model/contact.py
+++ model/contact.py
@@ -3,20 +3,23 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, first_name=None, middle_name=None, last_name=None, homephone=None, workphone=None, secondaryphone=None, id=None):
+    def __init__(self, first_name=None, middle_name=None, last_name=None, homephone=None, workphone=None,
+                 mobilephone=None, secondaryphone=None, id=None):
         self.first_name = first_name
         self.middle_name = middle_name
         self.last_name = last_name
         self.homephone = homephone
         self.workphone = workphone
+        self.mobilephone = mobilephone
         self.secondaryphone = secondaryphone
         self.id = id
 
     def __repr__(self):
-        return "%s:%s" % (self.id, self.first_name)
+        return "%s:%s %s" % (self.id, self.first_name, self.last_name)
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.first_name == other.first_name
+        return (self.id is None or other.id is None or self.id == other.id) and self.first_name == other.first_name and \
+               self.last_name == other.last_name
 
     def id_or_max(self):
         if self.id:
