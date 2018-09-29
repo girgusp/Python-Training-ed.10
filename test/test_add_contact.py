209397abe5bb0ac6f0575dcf3f1a4f4de7fa5b17
Przemysław Girguś <girgus.przemyslaw@gmail.com>
2018-09-29 13:51:54 +0200

    oddzielenie danych testowych od skrypty dla test_add_contact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,30 +2,16 @@
 
 from model.contact import Contact
 import pytest
-import random
-import string
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [
-    Contact(first_name=random_string("first_name", 10))
-    for first_name in ["", random_string("name", 10)]
-]
+from data.add_contact import constant as testdata
 
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
 def test_add_contact(app, contact):
-    pass
-    '''old_contacts = app.contact.get_contact_list()
-    contact = Contact(first_name="FN")
+    old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)'''
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
