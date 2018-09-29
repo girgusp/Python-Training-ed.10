c98210a128f40f91afb4e0367226c6a4327f0426
Przemysław Girguś <girgus.przemyslaw@gmail.com>
2018-09-29 14:42:24 +0200

    wygenerowane dane testowe w json dla test_add_contact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -8,6 +8,7 @@ from data.add_contact import constant as testdata
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
 def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
+    contact = Contact(first_name="first_name kontaktu", last_name="last_name kontaktu", middle_name="middle_name kontaktu")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
