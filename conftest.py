339c7532999c4b628474915bc728fad31e6a325c
Przemysław Girguś <girgus.przemyslaw@gmail.com>
2018-09-29 11:56:42 +0200

    update testu test_add_group

--- conftest.py
+++ conftest.py
@@ -34,6 +34,6 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--target", action="store", default="target.json") 
+    parser.addoption("--target", action="store", default="target.json")
 
 
